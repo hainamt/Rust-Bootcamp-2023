92da4748e4b9caa44e021d739fb1545fdb21cae1
Nam Tran <hainam.hp.99@gmail.com>
2023-07-10 17:07:22 +0700

    Finished Exercise2

---
+++ exercises/basic-of-rust/src/count_test.rs
@@ -0,0 +1,16 @@
+extern crate rand;
+use rand::Rng;
+
+fn count_characters(s: &str, c: char) -> usize {
+    s.chars().filter(|x| *x == c).count()
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    if rng.gen_bool(0.5) {
+        count_characters("Hello world", 'o');
+    } else {
+        count_characters("Hello world", 23);
+        
+    }
+}
--- exercises/ownership-borrowing/src/lib.rs
+++ exercises/ownership-borrowing/src/lib.rs
@@ -3,8 +3,8 @@
 fn exercise1() {
     // Use as many approaches as you can to make it work
     let x = String::from("hello, world");
-    let y = x;
-    let z = x;
+    let y = x.clone();
+    let z = &x;
 }
 
 // Exercise 2
@@ -17,8 +17,9 @@ fn exercise2() {
     println!("{}", s2);
 }
 // Only modify the code below!
-fn take_ownership(s: String) {
+fn take_ownership(s: String) -> String {
     println!("{}", s);
+    s
 }
 
 // Exercise 3
@@ -50,9 +51,10 @@ fn exercise3() {
 
 // Exercise 4
 // Make it compile
-fn exercise4(value: u32) -> &'static str {
+fn exercise4(value: u32) -> String {
     let str_value = value.to_string(); // Convert u32 to String
     let str_ref: &str = &str_value; // Obtain a reference to the String
+    let str_ref = str_ref.to_string();
     str_ref // Return the reference to the String
 }
 
@@ -64,12 +66,12 @@ fn exercise5() {
 
     let key = 3;
 
-    let res = match my_map.get(&key) {
-        Some(child) => child,
+    let res = match my_map.get(&key) { // get the key 3, and match it
+        Some(child) => child.clone(),
         None => {
             let value = "3.0".to_string();
-            my_map.insert(key, value);
-            &value // HERE IT FAILS
+            my_map.insert(key, value.clone());
+            value // HERE IT FAILS
         }
     };
 
@@ -82,14 +84,14 @@ fn exercise5() {
 use std::io;
 
 fn exercise6() {
-    let mut prev_key: &str = "";
+    let mut prev_key = String::from("");
 
     for line in io::stdin().lines() {
         let s = line.unwrap();
 
         let data: Vec<&str> = s.split("\t").collect();
         if prev_key.len() == 0 {
-            prev_key = data[0];
+            prev_key = data[0].to_string();
         }
     }
 }
@@ -97,11 +99,12 @@ fn exercise6() {
 // Exercise 7
 // Make it compile
 fn exercise7() {
-    let mut v: Vec<&str> = Vec::new();
+    let mut v: Vec<String> = Vec::new();
     {
         let chars = [b'x', b'y', b'z'];
         let s: &str = std::str::from_utf8(&chars).unwrap();
-        v.push(&s);
+        let s = s.to_string();
+        v.push(s);
     }
     println!("{:?}", v);
 }
@@ -109,7 +112,7 @@ fn exercise7() {
 // Exercise 8
 // Make it compile
 fn exercise8() {
-    let mut accounting = vec!["Alice", "Ben"];
+    let mut accounting = vec!["Alice".to_string(), "Ben".to_string()];
     
     loop {
         let mut add_input = String::from("");
@@ -125,7 +128,7 @@ fn exercise8() {
             continue;
         }
 
-        let person = add_vec[0];
+        let person = add_vec[0].to_string();
         accounting.push(person);
     }
 }
